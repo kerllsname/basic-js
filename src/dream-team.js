4b9a330e99995da8be3a5d3e0e1b07422516bcbe
kerllsname <kirillpotapovnow@gmail.com>
2021-03-28 18:50:09 +0300

    feat: complite team task

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,24 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  let code = [];
+  let arr = [];
+
+  if(Array.isArray(members) == false || members == []) return false
+
+  for(let j = 0; j < members.length; j++){
+    if(typeof members[j] === 'string'){
+      arr.push(members[j])
+    }
+  }
+ 
+  for(let k = 0; k < arr.length; k++){
+    arr[k] = arr[k].trim();
+  }
+
+  for(let i = 0; i < arr.length; i++){
+    code.push(arr[i].substr(0, 1).toUpperCase())
+  }
+  
+  return code.sort().join('');
 };
