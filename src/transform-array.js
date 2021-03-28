02686a28becd1625b90cad81246a2f5a55a11895
kerllsname <kirillpotapovnow@gmail.com>
2021-03-28 22:08:01 +0300

    feat: complete array task

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,22 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+  if(Array.isArray(arr) === false){
+    throw new Error;
+  }
+
+  let conditions = ['--discard-next', '--discard-prev', '--double-next', '--double-prev', '']
+  let newarr = [];
+
+  for (let i = 0; i < arr.length; i += 1) {
+    if (arr[i] === '--discard-next') {
+      i++;
+      newarr.push('');
+    } else if (arr[i] === '--discard-prev') newarr.pop();
+      else if (arr[i] === '--double-next' && i < arr.length - 1) newarr.push(arr[i + 1]);
+      else if (arr[i] === '--double-prev' && i - 1 >= 0) newarr.push(newarr[newarr.length - 1]);
+      else newarr.push(arr[i]);
+  };
+
+  return newarr.filter(item => !conditions.includes(item));
 };
